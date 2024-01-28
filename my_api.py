c65eb1f3c48ff986d8fb7dd2e0df83e5d2fee82a
nv259 <nguyenvietnhat.03@gmail.com>
2024-01-28 19:42:03 +0700

    finish multi_attrs retrieval

--- my_api.py
+++ my_api.py
@@ -1,3 +1,4 @@
+import math
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
@@ -17,6 +18,7 @@ import h5py
 
 import time
 from tqdm.auto import tqdm
+from collections import defaultdict
 
 
 class InputQuery(BaseModel):
@@ -24,7 +26,13 @@ class InputQuery(BaseModel):
     attrs:list
     # lsis:str = 'faiss'
     
-
+    
+class APIResponse(BaseModel):
+    ids:list = [int]
+    similarities:list = [float]
+    retrieval_time:float
+    
+    
 print("LOADING CONFIG", end=' ')
 cfg.merge_from_file('./config/FashionAI/FashionAI.yaml')
 cfg.merge_from_file('./config/FashionAI/s2.yaml')
@@ -36,15 +44,17 @@ extractor = FeatureExtractor(cfg)
 print("--- DONE!\n")
 
 print("LOADING DATABASE", end=' ')
-collection_id = joblib.load("collections/c_idxs.npy")
 collections = []
-with h5py.File("collections/data.h5", 'r') as data:
+# collections_id = []
+collection_id = joblib.load("collections/multi_attrs/c_idxs.npy")
+
+with h5py.File("collections/multi_attrs/data.h5", 'r') as data:
     for i in range(cfg.DATA.NUM_ATTRIBUTES):
         collection = data["attr" + str(i)][...]
         collections.append(collection)
 print("--- DONE!\n")
 
-print("CONSTRUCTING LSIS: KDTREE - LSH - FAISS")
+print("CONSTRUCTING LSIS: FAISS")
 kdtrees = []
 lshs = []
 index_flats = []
@@ -56,12 +66,12 @@ for i in tqdm(range(cfg.DATA.NUM_ATTRIBUTES), desc=str(i)):
     # for i in range(len(collection)):
     #     lsh.index(collection[i], extra_data=i)
 
-    index_flat = faiss.IndexFlatL2(collection.shape[1])
+    index_flat = faiss.IndexFlatL2(collections[i].shape[1])
     # if faiss.get_num_gpus() > 0:
     #     res = faiss.StandardGpuResources()
     #     index_flat = faiss.index_cpu_to_gpu(res, 0, index_flat)
-    index_flat.train(collection) 
-    index_flat.add(collection)
+    index_flat.train(collections[i]) 
+    index_flat.add(collections[i])
     
     # kdtrees.append(kdtree)
     # lshs.append(lsh)
@@ -79,31 +89,45 @@ async def home():
 @app.post("/submit")
 async def submit(input_query: InputQuery, k=50):
     start_time = time.time()
-
-    x = Image.open(input_query.img_path)
-    attrs = torch.tensor(input_query.attrs)    
+    
+    k= int(k)
+    x = Image.open(input_query.img_path) 
     
     multi_dists = []
     multi_ids = []
      
-    for attr_idx, use_attr in enumerate(attrs):
+    for attr_idx, use_attr in enumerate(input_query.attrs):
         if use_attr == True: 
             # extract feature corresponding to given attribute
-            feature = extractor(x, attr_idx).cpu().numpy()
+            feature = extractor(x, torch.tensor(attr_idx)).cpu().numpy()
 
             # calculating dists w.r.t current attribute
             dists, ids = index_flats[attr_idx].search(feature, k)
-            multi_dists.append(dists)
-            multi_ids.append(ids)
+            multi_dists.append(dists.squeeze())
+            multi_ids.append(collection_id[ids].squeeze())
     
-    print(len(multi_dists)) 
-    print(len(multi_ids))
-    
+    final_ranked_list = combine_multi_attrs(multi_ids, multi_dists)
      
     finish_time = time.time() 
-    print("execution time", finish_time - start_time)
+    retrieval_time = finish_time - start_time
     
-    return finish_time - start_time
+    return APIResponse(ids=[int(id) for id in final_ranked_list.keys()], 
+                       similarities=[float(similarity) for similarity in final_ranked_list.values()],
+                       retrieval_time=retrieval_time)
+
+
+def combine_multi_attrs(ids, dists):
+    ranked_list = defaultdict(int)
+    
+    for i in range(len(ids)):
+        for j in range(len(ids[i])):
+            sim = (2 - dists[i][j] ** 2)/2 
+            ranked_list[ids[i][j]] += sim
+
+    ranked_list = sorted(ranked_list.items(), key=lambda x:x[1], reverse=True)
+    ranked_list = dict(ranked_list)
+
+    return ranked_list
 
 
 if __name__ == "__main__":
