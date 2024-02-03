7fa46694ce408ae59764d9f3ce2c581253cecaea
Vo T Phuong Anh <109939103+vtpanh@users.noreply.github.com>
2024-02-03 19:52:23 +0700

    Add files via upload

---
+++ back-end-server/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const request = require("request");
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+app.use(express.static("uploads"));
+app.use(cors());
+
+// Set up multer for handling file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads'); // Specify the folder where you want to save the uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+// Serve the HTML form
+// (app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// })
+
+// Handle file upload
+app.post('/submit', upload.single("image"), async (req, res) => {
+  data = {
+    "img_path": req.protocol + "://" + req.get("host") + "/" + req.file.filename,
+    "attrs": req.body.attributes
+  }
+  await request.post(
+    uri="http://localhost:8001/submit",
+    { json: data },
+    function (error, response, body) {
+      console.log(body);
+    }
+  )
+  // res.send('Image uploaded successfully!');
+  res.json({"message": "image uploaded successfully!"});
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
